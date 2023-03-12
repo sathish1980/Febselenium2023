4c51793430e82ba456640fb74dd5d01f64560502
sathish1980 <kumar.sathish189@gmail.com>
2023-03-12 10:59:57 +0530

    new project

---
+++ eclipse-workspace/Feburarysel2023/src/main/java/feb/Feburarysel2023/App.java
@@ -0,0 +1,13 @@
+package feb.Feburarysel2023;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
